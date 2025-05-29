617795b0b45cc575a9ff89b6e88dca38c444d77c
kaqui-null <emanuelgomes24.08@gmail.com>
2025-05-28 21:18:58 -0300

    fix typos in dfs()

--- main.py
+++ main.py
@@ -17,13 +17,17 @@ class Graph:
 
     def new_edge(self, origin, destiny):
         self.new_vertice(origin)
-        self.new_neighbor(destiny)
+        self.new_vertice(destiny)
         self.vertices[origin].new_neighbor(self.vertices[destiny])
 
 class TopologicalSort:
-    def sort(self, vertices) -> list:
+    def __init__(self):
+        pass
+
+    def sort(self, graph) -> list:
         visited = set()
         stack = []
+        vertices = graph.vertices
 
         for vertice in vertices.values():
             if vertice not in visited:
@@ -33,30 +37,31 @@ class TopologicalSort:
 
     def dfs(self, vertice, visited, stack) -> int:
         visited.add(vertice)
-        for neighbor in Vertice.neighbors:
+        for neighbor in vertice.neighbors:
             if neighbor not in visited:
                 self.dfs(neighbor, visited, stack)
 
-        return Vertice.id
+        return vertice.id
 
-class Main:
-    def main():
-        pass
+def main():
+    exemplo_grafo1()
 
-    def exemplo_grafo1():
-        g = Graph()
-        g.new_edge(7, 11)
-        g.new_edge(7, 8)
-        g.new_edge(5, 11)
-        g.new_edge(3, 8)
-        g.new_edge(3, 10)
-        g.new_edge(11, 2)
-        g.new_edge(11, 9)
-        g.new_edge(11, 10)
-        g.new_edge(8, 9)
+def exemplo_grafo1():
+    g = Graph()
+    g.new_edge(7, 11)
+    g.new_edge(7, 8)
+    g.new_edge(5, 11)
+    g.new_edge(3, 8)
+    g.new_edge(3, 10)
+    g.new_edge(11, 2)
+    g.new_edge(11, 9)
+    g.new_edge(11, 10)
+    g.new_edge(8, 9)
 
-        print(g.vertices)
-        print(TopologicalSort.sort(g.vertices))
+    tSort = TopologicalSort()
+
+    print(g.vertices)
+    print(tSort.sort(graph=g))
 
 if __name__=="__main__":
-    Main.main()
+    main()
